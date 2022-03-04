f0a46b80bd4bf7f55e199d812d4e19c7ca6c44fd
qiang li <qxl0@yahoo.com>
2022-03-04 16:03:49 -0500

    Finished left of header

--- components/Header.js
+++ components/Header.js
@@ -17,17 +17,20 @@ import {
 
 const Header = () => {
   return (
-    <div className="flex">
-      <h1 className="text-5xl text-green-500">Header</h1>
+    <div className="flex items-center">
       <Image
         src="https://links.papareact.com/5me"
         width={40}
         height={40}
         layout="fixed"
       />
-      <div className="flex ml-2 items-center rounded-full">
-        <SearchIcon className="h-5 w-5 text-blue-500" />
-        <input type="text" placeholder="Search faceboo" />
+      <div className="ml-2 flex items-center rounded-full bg-gray-100 p-2">
+        <SearchIcon className="h-6  text-gray-600" />
+        <input
+          className="ml-2 flex items-center bg-transparent placeholder-gray-500 outline-none"
+          type="text"
+          placeholder="Search faceboo"
+        />
       </div>
     </div>
   );
