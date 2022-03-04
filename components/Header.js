19ec95d6b244decd6cd2e9cb4272fbcb8140be82
qiang li <qxl0@yahoo.com>
2022-03-04 15:19:34 -0500

    Header: icon, search, searchicon

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Image
+        src="https://links.papareact.com/5me"
+        width={40}
+        height={40}
+        layout="fixed"
+      />
+      <div>
+        <SearchIcon className="h-6" />
+        <input type="text" placeholder="Search faceboo" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
