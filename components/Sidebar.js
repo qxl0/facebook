5fbb7371929421dec18856f10684f849042eeaed
qiang li <qxl0@yahoo.com>
2022-03-05 14:11:40 -0500

    Added More Icons on Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -17,6 +17,12 @@ const Sidebar = () => {
   return (
     <div className="mt-5 max-w-[600px] p-2 xl:min-w-[300px]">
       <SidebarRow Icon={UsersIcon} title="Friends" />
+      <SidebarRow Icon={UserGroupIcon} title="Groups" />
+      <SidebarRow Icon={ShoppingBagIcon} title="MarketPlace" />
+      <SidebarRow Icon={DesktopComputerIcon} title="Watch" />
+      <SidebarRow Icon={CalendarIcon} title="Events" />
+      <SidebarRow Icon={ClockIcon} title="Memories" />
+      <SidebarRow Icon={ChevronDownIcon} title="See More" />
     </div>
   );
 };
