6ba72d2194b10f66b5ed60bfd384d8ff1593cc9a
qiang li <qxl0@yahoo.com>
2022-03-05 14:37:01 -0500

    Hard coded stores array

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div>
+      <div>
+        {/* Stores */}
+        <Stories />
+        {/* Inputbox */}
+        {/* Posts */}
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
---
+++ components/Stories.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const stories = [
+  {
+    name: "John Doe",
+    src: "https://links.papareact.com/zof",
+    profile: "https://links.papareact.com/l4v",
+  },
+  {
+    name: "Elon Musk",
+    src: "https://links.papareact.com/4zn",
+    profile: "https://links.papareact.com/kxk",
+  },
+  {
+    name: "Jeff Bezos",
+    src: "https://links.papareact.com/k2j",
+    profile: "https://links.papareact.com/f0p",
+  },
+  {
+    name: "Mark Zuckerberg",
+    src: "https://links.papareact.com/xql",
+    profile: "https://links.papareact.com/snf",
+  },
+  {
+    name: "Bill Gates",
+    src: "https://links.papareact.com/4u4",
+    profile: "https://links.papareact.com/zvy",
+  },
+];
+
+function Stories() {
+  return <div>Stories</div>;
+}
+
+export default Stories;
