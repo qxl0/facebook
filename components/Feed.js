c690016a5ef651d439f2f8d6ce2511967c95cec8
qiang li <qxl0@yahoo.com>
2022-03-06 11:55:31 -0500

    Added react-firebase-hooks, posts component

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InputBox from "./InputBox";
+import Posts from "./Posts";
 import Stories from "./Stories";
 
 const Feed = () => {
@@ -11,6 +12,7 @@ const Feed = () => {
         {/* Inputbox */}
         <InputBox />
         {/* Posts */}
+        <Posts />
       </div>
     </div>
   );
