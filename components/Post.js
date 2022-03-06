433c44e61dc97edea4cc5ac60078eccbbb105c5b
qiang li <qxl0@yahoo.com>
2022-03-06 13:22:24 -0500

    Styled Post

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Post = ({ id, name, message, email, timestamp, image, postImage }) => {
-  return <div>Post</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="mt-5 rounded-t-2xl bg-white p-5  shadow-sm">
+        <div className="flex items-center space-x-2">
+          <img src={image} width={40} height={40} alt="" />
+          <div>
+            <p className="font-medium">{name}</p>
+            <p className="text-xs text-gray-400">
+              {new Date(timestamp?.toDate()).toLocaleString()}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
--- components/Posts.js
+++ components/Posts.js
@@ -1,12 +1,29 @@
 import { orderBy, query } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
+import { collection } from "firebase/firestore";
+import { db } from "../firebase";
+import Post from "./Post";
 
 const Posts = () => {
   const [realtimePosts, loading, error] = useCollection(
     query(collection(db, "posts"), orderBy("timestamp", "desc"))
   );
 
-  return <div>Posts</div>;
+  return (
+    <div>
+      {realtimePosts?.docs.map((post) => (
+        <Post
+          key={post.id}
+          name={post.data().name}
+          message={post.data().message}
+          email={post.data().email}
+          timestamp={post.data().timestamp}
+          image={post.data().image}
+          postImage={post.data().postImage}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
