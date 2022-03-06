5772cccaebcc4edccec98a22f489b4c0589e55e6
qiang li <qxl0@yahoo.com>
2022-03-06 13:06:24 -0500

    Added Post component

---
+++ components/Post.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Post = ({ id, name, message, email, timestamp, image, postImage }) => {
+  return <div>Post</div>;
+};
+
+export default Post;
--- components/Posts.js
+++ components/Posts.js
@@ -1,6 +1,11 @@
-import React from "react";
+import { orderBy, query } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 const Posts = () => {
+  const [realtimePosts, loading, error] = useCollection(
+    query(collection(db, "posts"), orderBy("timestamp", "desc"))
+  );
+
   return <div>Posts</div>;
 };
 
