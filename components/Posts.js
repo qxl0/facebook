894556ef694bf440c31bccc9f270848e6a10cab4
qiang li <qxl0@yahoo.com>
2022-03-06 15:04:59 -0500

    Datashape is different returned from SS

--- components/Posts.js
+++ components/Posts.js
@@ -24,18 +24,22 @@ const Posts = ({ posts }) => {
               postImage={post.data().postImage}
             />
           ))
-        : posts.map((post) => (
-            // console.log("postImage: ", post.data().postImage),
-            <Post
-              key={post.id}
-              name={post.data().name}
-              message={post.data().message}
-              email={post.data().email}
-              timestamp={post.data().timestamp}
-              image={post.data().image}
-              postImage={post.data().postImage}
-            />
-          ))}
+        : posts.map(
+            (post) => (
+              console.log("post SSR: ", post),
+              (
+                <Post
+                  key={post.id}
+                  name={post.name}
+                  message={post.message}
+                  email={post.email}
+                  timestamp={post.timestamp}
+                  image={post.image}
+                  postImage={post.postImage}
+                />
+              )
+            )
+          )}
     </div>
   );
 };
