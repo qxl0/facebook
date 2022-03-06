febfbd8b09ff9cba34e215b2a90d2d5371cbfb9d
qiang li <qxl0@yahoo.com>
2022-03-06 14:35:18 -0500

    Added tailwind-scrollbar-hide and applied to Feed

--- components/Feed.js
+++ components/Feed.js
@@ -5,7 +5,11 @@ import Stories from "./Stories";
 
 const Feed = () => {
   return (
-    <div className="mr-4 h-screen flex-grow overflow-y-auto pb-44 pt-6 xl:mr-40">
+    <div
+      className="mr-4 h-screen flex-grow 
+    overflow-y-auto
+    pb-44 pt-6 scrollbar-hide xl:mr-40"
+    >
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
         {/* Stores */}
         <Stories />
--- components/Widgets.js
+++ components/Widgets.js
@@ -1,7 +1,32 @@
 import React from "react";
+import {
+  DotsHorizontalIcon,
+  SearchIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/solid";
 
+const contacts = [
+  { src: "https://links.papareact.com/f0p", name: "Jeff Bezos" },
+  { src: "https://links.papareact.com/kxk", name: "Elon Musk" },
+  { src: "https://links.papareact.com/zvy", name: "Bill Gates" },
+  { src: "https://links.papareact.com/5nf", name: "Mark Zuckerberg" },
+  { src: "https://links.papareact.com/d0c", name: "Harry Potter" },
+  { src: "https://links.papareact.com/6gg", name: "The Queen" },
+  { src: "https://links.papareact.com/r57", name: "James Bond" },
+];
 const Widgets = () => {
-  return <div>Widgets</div>;
+  return (
+    <div className="mt-5 hidden w-60 flex-col p-2 lg:flex">
+      <div className="mb-5 flex items-center justify-between text-gray-500">
+        <h2 className="text-xl">Contacts</h2>
+        <div className="flex space-x-2">
+          <VideoCameraIcon className="h-6" />
+          <SearchIcon className="h-6" />
+          <DotsHorizontalIcon className="h-6" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Widgets;
