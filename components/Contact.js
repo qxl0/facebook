48171a5946be0e83fec436d0db8d3a774fd088c9
qiang li <qxl0@yahoo.com>
2022-03-06 14:43:01 -0500

    Contacts build now very nice

--- components/Contact.js
+++ components/Contact.js
@@ -1,7 +1,21 @@
+import Image from "next/image";
 import React from "react";
 
-const Contact = () => {
-  return <div>Contact</div>;
+const Contact = ({ src, name }) => {
+  return (
+    <div className="relative mb-2 flex cursor-pointer items-center space-x-3 rounded-xl p-2 hover:bg-gray-200">
+      <Image
+        className="rounded-full"
+        objectFit="cover"
+        src={src}
+        width={50}
+        height={50}
+        layout="fixed"
+      />
+      <p>{name}</p>
+      <div className="absolute bottom-2 left-7 h-3 w-3 animate-bounce rounded-full bg-green-400"></div>
+    </div>
+  );
 };
 
 export default Contact;
